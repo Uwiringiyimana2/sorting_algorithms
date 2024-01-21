363bc5e33601ad15de642711b76634ffbed267c2
Eric Uwiringiyimana <uwiringiyimanaericmax2000@gmail.com>
2024-01-21 15:47:45 +0200

    1-insertion_sort_list.c

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,7 +1,7 @@
 #include "sort.h"
 
 /**
- * swap - swap two nodes in a listint_t doubly-linked list.
+ * swap_node - swap two nodes in a listint_t doubly-linked list.
  * @h: A pointer to the head of the doubly-linked list
  * @n1: A pointer to the first node to swap.
  * @n2: The second node to swap
